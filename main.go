52801ba8bac90c51ff2ed587b112d682f271817c
Avelino Sanchez <avelino.schz@gmail.com>
2022-04-03 16:25:08 -0500

    feat(api): add base http handlers

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	log.Print("configuring application")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("/")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Server is happy!"))
+	})
+
+	http.HandleFunc("/current", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	http.HandleFunc("/next", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	http.HandleFunc("/previous", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	log.Println("starting server on port :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal()
+	}
 }
